a32456baca759f7bd13213c1c0e7d33bee4dd8c5
Fabian Loaeza <floaeza@bbinco.com>
2023-10-08 20:47:39 -0700

    Contruccion about page

--- src/app/bbinco/pages/about/about.component.ts
+++ src/app/bbinco/pages/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from '../../animations/fade-animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations: [fadeAnimation]
 })
 export class AboutComponent {
 
