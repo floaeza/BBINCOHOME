07226a16b460f25f26cd26239c5b2fd2887aae1b
Fabian Loaeza Morales <floaeza@gmail.com>
2023-10-17 15:35:23 -0700

    team page terminada

--- src/app/bbinco/pages/team/team.component.ts
+++ src/app/bbinco/pages/team/team.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from '../../animations/fade-animations';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
+  animations: [fadeAnimation]
 })
-export class TeamComponent {
-
+export class TeamComponent implements OnInit{
+  ngOnInit(): void {
+    AOS.init({
+      duration: 800,
+      offset: 350
+    });
+  }
 }
